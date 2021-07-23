fd4c5636abc0f031e143cfb1970717d5f2a9f0e1
Felix Niu <felix@interactivelabs.co>
2021-07-23 18:11:02 +0800

    Add more shorter names for layout guides

--- Source/UIView+Utilities.swift
+++ Source/UIView+Utilities.swift
@@ -36,3 +36,55 @@ public extension UIView {
     var flCenterX: NSLayoutXAxisAnchor { centerXAnchor }
     var flCenterY: NSLayoutYAxisAnchor { centerYAnchor }
 }
+
+public extension UIView {
+    var flSafeLeft: NSLayoutXAxisAnchor { safeAreaLayoutGuide.leftAnchor }
+    var flSafeRight: NSLayoutXAxisAnchor { safeAreaLayoutGuide.rightAnchor }
+
+    var flSafeLeading: NSLayoutXAxisAnchor { safeAreaLayoutGuide.leadingAnchor }
+    var flSafeTrailing: NSLayoutXAxisAnchor { safeAreaLayoutGuide.trailingAnchor }
+
+    var flSafeTop: NSLayoutYAxisAnchor { safeAreaLayoutGuide.topAnchor }
+    var flSafeBottom: NSLayoutYAxisAnchor { safeAreaLayoutGuide.bottomAnchor }
+
+    var flSafeWidth: NSLayoutDimension { safeAreaLayoutGuide.widthAnchor }
+    var flSafeHeight: NSLayoutDimension { safeAreaLayoutGuide.heightAnchor }
+
+    var flSafeCenterX: NSLayoutXAxisAnchor { safeAreaLayoutGuide.centerXAnchor }
+    var flSafeCenterY: NSLayoutYAxisAnchor { safeAreaLayoutGuide.centerYAnchor }
+}
+
+public extension UIView {
+    var flMarginsLeft: NSLayoutXAxisAnchor { layoutMarginsGuide.leftAnchor }
+    var flMarginsRight: NSLayoutXAxisAnchor { layoutMarginsGuide.rightAnchor }
+
+    var flMarginsLeading: NSLayoutXAxisAnchor { layoutMarginsGuide.leadingAnchor }
+    var flMarginsTrailing: NSLayoutXAxisAnchor { layoutMarginsGuide.trailingAnchor }
+
+    var flMarginsTop: NSLayoutYAxisAnchor { layoutMarginsGuide.topAnchor }
+    var flLayoutMarginsBottom: NSLayoutYAxisAnchor { layoutMarginsGuide.bottomAnchor }
+
+    var flMarginsWidth: NSLayoutDimension { layoutMarginsGuide.widthAnchor }
+    var flMarginsHeight: NSLayoutDimension { layoutMarginsGuide.heightAnchor }
+
+    var flMarginsCenterX: NSLayoutXAxisAnchor { layoutMarginsGuide.centerXAnchor }
+    var flMarginsCenterY: NSLayoutYAxisAnchor { layoutMarginsGuide.centerYAnchor }
+}
+
+public extension UIView {
+    var flReadableLeft: NSLayoutXAxisAnchor { readableContentGuide.leftAnchor }
+    var flReadableRight: NSLayoutXAxisAnchor { readableContentGuide.rightAnchor }
+
+    var flReadableLeading: NSLayoutXAxisAnchor { readableContentGuide.leadingAnchor }
+    var flReadableTrailing: NSLayoutXAxisAnchor { readableContentGuide.trailingAnchor }
+
+    var flReadableTop: NSLayoutYAxisAnchor { readableContentGuide.topAnchor }
+    var flReadableBottom: NSLayoutYAxisAnchor { readableContentGuide.bottomAnchor }
+
+    var flReadableWidth: NSLayoutDimension { readableContentGuide.widthAnchor }
+    var flReadableHeight: NSLayoutDimension { readableContentGuide.heightAnchor }
+
+    var flReadableCenterX: NSLayoutXAxisAnchor { readableContentGuide.centerXAnchor }
+    var flReadableCenterY: NSLayoutYAxisAnchor { readableContentGuide.centerYAnchor }
+}
+
